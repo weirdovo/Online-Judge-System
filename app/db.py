2cdde2cd23163f2d7e8127454637fb9ce76c9d15
Liu_wt <liuwt24@mails.tsinghua.edu.cn>
2025-07-14 01:52:40 +0800

    feat(db): intialize database

---
+++ app/db.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE = "sqlite:///./data.db"
+engine = create_engine(DATABASE, connect_args = {"check_same_thread" : False})
+Localsession = sessionmaker(bind = engine, autoflush = False, autocommit = False)
+
+Base = declarative_base()
