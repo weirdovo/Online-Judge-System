2bb776b62a0105fa56c526b697da07121bcce615
Liu_wt <liuwt24@mails.tsinghua.edu.cn>
2025-07-14 01:54:02 +0800

    feat(problems): implement problem management API

--- app/problems.py
+++ app/problems.py
@@ -1,66 +1,51 @@
-from fastapi import APIRouter, Request
-from pydantic import BaseModel
-from typing import Optional, List
-
-from app.utils import make_response, admin_guard
+from fastapi import APIRouter, Request, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
+from app.schemas import Problem_
+from app.models import Problem
+from app.utils import make_response, admin_guard, get_db
 
 router = APIRouter()
-problem_set = {}  # problem_id : problem (class Problem)
-
-class Cases(BaseModel):
-    input : str
-    output : str
-
-# Correct format of problem
-class Problem(BaseModel):
-    id: str
-    title: str
-    description: str
-    input_description: str
-    output_description: str
-    samples: List[Cases]
-    constraints: str
-    testcases: List[Cases]
-    hint: Optional[str] = None
-    source: Optional[str] = None
-    tags: Optional[List[str]] = None
-    time_limit: Optional[float] = None
-    memory_limit: Optional[int] = None
-    author: Optional[str] = None
-    difficulty: Optional[str] = None
-    
 
 @router.get("/api/problems/")
-async def list_problems(request : Request):
-    if "user_id" not in request.session:
-        return make_response(401, "not logged in", None)
-    return make_response(200, "success", [{"id" : x.id, "title" : x.title} for x in problem_set.values()])
+async def list_problems(request : Request, db : Session = Depends(get_db)):
+    # if "user_id" not in request.session:
+    #     return make_response(401, "not logged in", None)
+    return make_response(200, "success", 
+                         [{"id" : x.id, "title" : x.title} for x in db.query(Problem).all()])
     
 @router.post("/api/problems/")
-async def add_problem(problem: Problem, request: Request):
+async def add_problem(problem : Problem_, request : Request, db : Session = Depends(get_db)):
     if "user_id" not in request.session:
         return make_response(401, "not logged in", None)
-    
-    if problem.id in problem_set:
-        return make_response(409, "id already existed", None)
-    else:
-        problem_set[problem.id] = problem
+    new_problem = Problem(**problem.model_dump())
+    db.add(new_problem)
+    try:
+        db.commit()
+        db.refresh(new_problem)
         return make_response(200, "add success", {"id" : f"{problem.id}"})
+    except IntegrityError:
+        db.rollback() 
+        return make_response(409, "id already existed", None)
 
 @router.delete("/api/problems/{id}")
-async def delete_problem(id: str, request: Request):
+async def delete_problem(id : str, request : Request, db : Session = Depends(get_db)):
     if not admin_guard(request):
         return make_response(401, "permission denied", None)
-    if id in problem_set:
-        del problem_set[id]
-        return make_response(200, "delete success", {"id" : id})
-    else:
+    problem = db.query(Problem).filter_by(id = id).first()
+    if not problem:
         return make_response(404, "problem does not exist", None)
+    db.delete(problem)
+    db.commit()
+    return make_response(200, "delete success", {"id" : id})
+
 
 @router.get("/api/problems/{id}")
-async def get_problem(id):
-    if id in problem_set:
-        return make_response(200, "success", problem_set[id].dict())
-    else:
+async def get_problem(id : str, db : Session = Depends(get_db)):
+    
+    problem = db.query(Problem).filter_by(id = id).first()
+    if not problem:
         return make_response(404, "problem does not exist", None)
+    return make_response(200, "success", problem.to_dict())
+
     
