6d3f273d0237225aa28cd7758e6af2134879bc7e
Liu_wt <liuwt24@mails.tsinghua.edu.cn>
2025-07-14 22:14:04 +0800

    feat(judge): implement judgement of submission

--- app/judge.py
+++ app/judge.py
@@ -4,15 +4,53 @@ import threading
 import psutil
 import time
 import os
+import asyncio
 
-def judge(case, language : str, tl : float, ml : int):
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from app.models import Problem, Submission
+from app.utils import get_db
+
+async def run_judge(id : int, code : str, db : Session = Depends(get_db)):
+    task = db.query(Submission).filter_by(id = id).first()
+    problem = db.query(Problem).filter_by(id = task.problem_id).first()
+    try:
+        with tempfile.NamedTemporaryFile(mode="w+", delete=False, suffix=".py") as tmp_code:
+            tmp_code.write(code)
+            tmp_code.flush()
+            code_path = tmp_code.name
+        score = 0
+        detail = []
+        counts = len(problem.testcases) * 10
+        for i, case in enumerate(problem.testcases):
+            status = await asyncio.to_thread(judge_case, case, task.language, 
+                                             problem.time_limit, problem.memory_limit, code_path)
+            status["id"] = i+1
+            if status["status"] == "AC":
+                score = score + 10
+            detail.append(status)
+        task.status = "success"
+        task.counts = counts
+        task.score = score
+        task.detail = detail    
+        db.commit()
+        db.refresh(task)
+    except Exception as e:
+        task.status = "error"
+        db.commit()
+        db.refresh(task)
+    finally:
+        if 'code_path' in locals() and os.path.exists(code_path):
+            os.remove(code_path)
+
+def judge_case(case, language : str, tl : float, ml : int, code_path : str):
     result_holder = {"status" : "OK", "time" : 0.0, "memory" : 0}
     with tempfile.NamedTemporaryFile(mode = 'w+', delete = False) as tmp_in:
         tmp_in.write(case["input"])
         tmp_in.flush()
         try:
             proc = subprocess.Popen(
-                [language, "test_submission.py"],
+                [language, code_path],
                 stdin=open(tmp_in.name, "r"),
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE
@@ -26,7 +64,7 @@ def judge(case, language : str, tl : float, ml : int):
             try:
                 stdout, stderr = proc.communicate(timeout = tl)
                 end = time.time()
-                result_holder["time"] =round(start - end)
+                result_holder["time"] =round(end - start, 3)
                 output = stdout.decode().strip()
                 if result_holder["status"] != "OK":
                     return result_holder
@@ -45,7 +83,6 @@ def judge(case, language : str, tl : float, ml : int):
                 result_holder["time"] = tl
             finally:
                 memory_monitor.join()
-            
         except Exception:
             result_holder["status"] = "UNK"
         finally:
