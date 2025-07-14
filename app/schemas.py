ca782124f1907e316f765e1376e733593aebf224
Liu_wt <liuwt24@mails.tsinghua.edu.cn>
2025-07-14 22:17:20 +0800

    feat(schemas): add submission schemas

--- app/schemas.py
+++ app/schemas.py
@@ -29,3 +29,26 @@ class Problem_(BaseModel):
     memory_limit: Optional[int] = None
     author: Optional[str] = None
     difficulty: Optional[str] = None
+    
+class submission(BaseModel):
+    problem_id : str
+    language : str
+    code : str
+    
+class submission_list(BaseModel):
+    user_id : Optional[int] = None
+    problem_id : Optional[str] = None
+    status : Optional[str] = "success"
+    page : Optional[int] = None
+    page_size : Optional[int] = None
+    
+    def valid_params(self):
+        if self.user_id is None and self.problem_id is None:
+            return False
+        if self.page is None:
+            self.page = 1
+        elif self.page is not None and self.page_size is None:
+            return False
+        if self.status not in ["success", "pending", "error"]:
+            return False
+        return True
